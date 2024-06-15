567fb6643497db0e6676a5096c6b187f484df0d7
federicogfb <fedelibra98@gmail.com>
2024-06-15 15:40:58 -0300

    Add develop custom message.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Servidor de develop. Aca van las nuevas features."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
