c5d619d162593fd88530f47c6685a248ea343f4b
federicogfb <fedelibra98@gmail.com>
2024-06-15 15:44:03 -0300

    Add QA custom message.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Estas en QA! Rompe este sv como quieras :)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
